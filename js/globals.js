5fd3398c800b7ca22d048036b99149743e98f84c
Ackou Jens <ackoujens@gmail.com>
2016-11-08 14:32:05 +0100

    Cleanup

--- js/globals.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-var USERNAME = 'ackoujens', // github username
-    REPO = 'volvo-850-wagon', // github repository
-    TOKEN = 'd4df910ddeb8ffe955de7cff6f92b941e7dfa79f'; // github app access token
